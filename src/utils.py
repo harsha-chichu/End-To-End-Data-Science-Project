017a8dda6b32eb6729bdfe9b88e22cfe2c29824c
harsha-chichu <harshachinnu129@gmail.com>
2025-11-12 17:22:41 +0530

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -24,13 +24,18 @@ def save_object(file_path, obj):
         logging.error(f"Error saving object at {file_path}: {e}")
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-    
+            para=params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
@@ -42,7 +47,6 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
-
         return report
     
     except Exception as e:
